56e0b22a9abb5c20628ae630b9152f793b2dd141
MJ Miranda <mjmiranda@gmail.com>
2017-03-04 14:05:21 -0800

    Changes to page navigation, templates, navbar

--- web/uploads/urls.py
+++ web/uploads/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^dq/$', views.dq, name='dq'),
+    url(r'^dq/$', views.dq, name='dq'),   #home
+    url(r'^about$', views.about, name='about'),
+    url(r'^contact$', views.contact, name='contact'),
+    url(r'^dq/results$', views.results, name='results'),
 ]
--- web/uploads/views.py
+++ web/uploads/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 from uploads.models import Document
 from uploads.forms import DocumentForm
@@ -15,16 +15,34 @@ def dq(request):
         #this is the part of the view that gets called when someone sees the below upload dialog and attempts to POST a file
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
-            something = request.FILES['file']
-            filename = something.name
-            docfile = Document(file=something, filename=filename, creation_date=datetime.datetime.now())           
+            # do we really want to write to disk just in case?
+            form_file = request.FILES['file']
+            filename = form_file.name
+            docfile = Document(file=form_file, filename=filename, creation_date=datetime.datetime.now())           
             print(docfile.creation_date)
             print(docfile.filename)
             destination = 'tmp/' + filename
-            filehandler.write_file_to_disk(something, destination)
-        return HttpResponse('file written to ' + destination)
+            filehandler.write_file_to_disk(form_file, destination)
+
+            #process file: maybe this will be the handler for showing the progress bar or something
+            #filehandler.process_file(form_file)
+    
+            #return HttpResponse('file written to ' + destination)
+            return render(request, 'uploads/results.html', {'form':form})
     else:
         #this is the part of the view that is brought up first (to show file upload dialog)
         form = DocumentForm()
     return render(request, 'uploads/dq.html', {'form':form})
         
+def results(request):
+    return render(request, 'uploads/results.html', {'form':form})
+
+def home(request):
+    form = DocumentForm()
+    return render(request, 'uploads/dq.html', {'form':form})
+
+def about(request):
+    return render(request, 'uploads/about.html', {})
+
+def contact(request):
+    return render(request, 'uploads/contact.html', {})
