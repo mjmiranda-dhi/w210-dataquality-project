9599166e47f82bbf1bb2ca59ce908aab967a0ef6
MJ Miranda <mjmiranda@gmail.com>
2017-04-16 14:02:25 -0700

    incorporated graph generation viz, now displaying preprocessing viz

--- web/uploads/views.py
+++ web/uploads/views.py
@@ -200,14 +200,173 @@ def preprocess_data_viz(request):
                             #NOTE: need to write to uploads/static
     plotly.offline.plot(table, filename='uploads/static/img/viz_pre_table.html', auto_open=False)
     #TODO eventually this needs to be loaded and displayed, how????
-    time.sleep(10)    
+    #time.sleep(10)    
+
+
+    #### CREATE GRAPH
+    ##############################################
+    #####        Graph Model        ##############
+    #############################################
+
+    tracker = {}
+    #levels_v2: A flat list of values for labeling
+    levels_v2 =  [item for sublist in levels for item in sublist]
+
+    root_node_list = []
+    root_node_counter = 0
+    edge_list = []
+
+    for i in range(len(tree_data)):
+        temp = tree_data[i].split("**")
+    #current_data = filter(None, current_data)
+        current_data = []
+        for i in temp:
+            if i != '':
+                current_data.append(i)
+  
+##Update
+        if current_data[0] not in root_node_list and current_data[0] != '':
+            root_node_list.append(current_data[0])
+            tracker[root_node_counter] = [current_data[0]]
+            root_node_counter += 1
+  
+  
+        location = 0
+        for j in range(len(current_data) - 1):
+    
+        #Incrementing location to match the number with the 
+        #Hireachical path
+            if j > 0:
+                location += len(levels[j-1])
+    
+        #Find location in the list to update the coordinate for the pair
+   
+            a = levels[j].index(current_data[j]) + location
+            b = levels[j+1].index(current_data[j+1]) + location + len(levels[j])
+    
+            pairs = (a, b)
+
+            if pairs not in edge_list:
+                edge_list.append(pairs)
+      #tracker.append(current_data[:j+1])
+    
+    #Find way to find first records
+            if b not in tracker:
+                tracker[b] = current_data[:j+2]
+
+    ################
+    total_category = len(levels_v2)
+    #v_label = map(str, range(total_category))
+
+    v_label = []
+    for i in range(1,total_category+1):
+        v_label.append(str(i))
+    
+    G = Graph(n = total_category, directed=True)
+
+    #edge_list
+    G.add_edges(edge_list)
+    lay = G.layout()
+    position = {k: lay[k] for k in range(total_category)}
+
+
+    Y = [position[k][1] for k in range(total_category)]
+    M = max(Y)
+
+
+    #There are edges connecting the vertices
+    es = EdgeSeq(G) # sequence of edges
+    E = [e.tuple for e in G.es] # list of edges
+
+    L = len(position)
+    Xn = [position[k][0] for k in range(L)]
+    Yn = [3*M-position[k][1] for k in range(L)]  #2
+    Xe = []
+    Ye = []
+
+    for edge in E:
+        Xe+=[position[edge[0]][0],position[edge[1]][0], None]
+        Ye+=[3*M-position[edge[0]][1],3*M-position[edge[1]][1], None] 
+
+    labels = v_label
+    #### END CREATE GRAPH
+
+    long_name = []
+    for i in range(len(tracker)):
+        long_name.append(' - '.join(tracker[i]))  
+
+    ###############################################
+    lines = go.Scatter(x=Xe,
+                       y=Ye,
+                       mode='lines',
+                       line=dict(color='rgb(255,255,0)', width=1),  #rgb(210,210,210)/ 128X3 2nd best
+                       hoverinfo='none'
+                       )
+
+    dots = go.Scatter(x=Xn,
+                      y=Yn,
+                      mode='markers',
+                      name='',
+                      marker=dict(symbol='dot',
+                      size=14, 
+                      color='#606060',    #'#DB4551', #6175c1
+                      line=dict(color='rgb(32,32,32)', width=1)
+                      ),
+                      text= long_name,  #levels_v2,
+                      #Any combination of "x", "y", "z", "text", "name" joined with a "+" OR "all" or "none" or "skip".
+                      textfont={"size":15},
+                      hoverinfo='text+name',  
+                      opacity=0.9)
+###########################
+    axis = dict(showline=False, # hide axis line, grid, ticklabels and  title
+                zeroline=False,
+                showgrid=False,
+                showticklabels=False,)
+
+    layout = dict(title= 'Overall Data Structure',  
+                  annotations=make_annotations(position, v_label,labels,M,position),
+                  font=dict(size=12),
+                  showlegend=False,
+                  xaxis=go.XAxis(axis),
+                  yaxis=go.YAxis(axis),          
+                  margin=dict(l=40, r=40, b=85, t=100),  #dict(l=40, r=40, b=85, t=100)
+                  hovermode='closest',
+                  plot_bgcolor='rgb(64,64,64)')   
+              
+
+    data=go.Data([lines, dots])
+    fig=dict(data=data, layout=layout)
+    fig['layout'].update(annotations=make_annotations(position, levels_v2, labels,M,position)) #v_label
+
+    #iplot(fig, filename='Graph_Plot')
+
+    plotly.offline.plot(fig, filename='uploads/static/img/viz_pre_tree_graph.html', auto_open=False)
 
     data = {
         # NOTE: to display, we need to use /static/img not uploads/static/img
-        'pre_viz': '/static/img/viz_pre_table.html',
-        'pre_stats': 'tmp/viz_pre_table.html',
+        'pre_stats': '/static/img/viz_pre_table.html',
+        'pre_viz': '/static/img/viz_pre_tree_graph.html',
         'html': '<div>Hello World</div>'
     }
+    time.sleep(15)
 
     print(data)
     return data
+
+
+#### FOR GRAPH
+def make_annotations(pos, text, labels, M,position, font_size=7, font_color='rgb(250,250,250)'):
+    L=len(pos)
+    #if len(textv)!=L:
+        #raise ValueError('The lists pos and text must have the same len')
+    annotations = go.Annotations()
+    for k in range(L):
+        annotations.append(
+            go.Annotation(
+                text=labels[k], # or replace labels with a different list for the text within the circle  
+                x=pos[k][0], y=3*M-position[k][1],
+                xref='x1', yref='y1',
+                font=dict(color=font_color, size=font_size),
+                showarrow=False)
+        )
+    return annotations 
